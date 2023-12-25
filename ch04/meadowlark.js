1365155fa6262ec111aec02ee2ef0754d430b8bb
chipchippp <locnvth2209036@fpt.edu.vn>
2023-12-25 14:32:33 +0700

    ch04

--- ch04/meadowlark.js
+++ ch04/meadowlark.js
@@ -1,32 +1,32 @@
 const express = require('express')
-const exphbs = require('express-handlebars');
+const expressHandlebars = require('express-handlebars').engine;
 const fortune = require('./lib/fortune.js')
 const app = express()
+const port = process.env.port || 3000
 
-// configure Handlebars view engine
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main',
+
+//configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+    defaultlayout: 'main'
 }))
-
 app.set('view engine', 'handlebars')
-
-const port = process.env.PORT || 3000
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res)=> res.render('home', {title: 'Home Page', message: 'Welcome to our website'}))
-app.get('/about', (req, res)=> res.render('about', {fortune: fortune.getFortune()}))
+app.get('/about',(req,res)=>{
+    res.render('about', {fortune: fortune.getFortune()})
+})
 
 
 //custom 404 page
 app.use((req, res)=> {
-    res.status(404).render('404')
+    res.status(404).render('404', {title : '404 not found'})
 })
 
 // custom 500 page
 app.use((err, req, res, next)=>{
     console.error(err.message)
-    res.status(500).render('500')
+    res.status(500).render('500', {title : '500 Internal Servel Error'})
 })
-
 app.listen(port, () => console.log(
     `Server started on http://localhost:${port}`))
