b824d7b1d731d2c054bd5d1dc251d039ee931e0a
chipchippp <locnvth2209036@fpt.edu.vn>
2024-01-03 14:38:50 +0700

    crud

---
+++ crud-with-mongodb/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const blogRouter = require('./routes/BlogRoutes')
+const app = express();
+const port = 3000;
+
+// middleware
+
+app.use(express.json());
+app.use("/api/blogs", blogRouter);
+
+// app.listen(3001, ()=>{
+//     console.log("server is...")
+// })
+
+const mongoose = require('mongoose')
+//
+// mongoose.connect("mongodb://localhost:27017/crud",
+//     (err) => {
+//         if(err){
+//             console.log(err);
+//         } else{
+//             console.log("Connected to MongoDB");
+//         }
+//     }
+// );
+mongoose.connect('mongodb://localhost/crud')
+  .then(() => {
+    console.log('Đã kết nối đến MongoDB');
+    // Phần còn lại của mã của bạn...
+  })
+  .catch((err) => {
+    console.error('Lỗi kết nối đến MongoDB:', err);
+  });
+module.exports = app;
+
+app.listen(port, () =>
+console.log(`Server started on http://localhost:${port}/api/blogs`))
